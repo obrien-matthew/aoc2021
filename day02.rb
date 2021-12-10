6c8b1d8ead07caf126054201fb5aaceb704bd50d
Matthew OBrien <obrien.mlotwis@gmail.com>
2021-12-10 14:16:40 -0500

    day2

--- day02.rb
+++ day02.rb
@@ -1,13 +1,14 @@
-x=y=0
+aim=x=y=0
 File.foreach("2021.02.input.txt") {
     |line|
     op, units = line.split
-    if op == "forward":
+    if op == "forward"
         x+=units.to_i
-    elsif op == "down":
-        y-=units.to_i
-    elsif op == "up":
-        y+=units.to_i
+        y+=(units.to_i*aim)
+    elsif op == "down"
+        aim+=units.to_i
+    elsif op == "up"
+        aim-=units.to_i
     end
 }
 puts x*y
